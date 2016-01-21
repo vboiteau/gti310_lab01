2fd921602c461546b02eb4ab3a05f66b9c6673e8
Vincent Boiteau-Robert <vboiteau94@gmail.com>
2016-01-21 14:22:19 -0500

    Feature for the model class of a bmp image

---
+++ src/model/BMPImage.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.awt.image.BufferedImage;
+
+public class BMPImage extends Image {
+
+	@Override
+	public BufferedImage draw() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
