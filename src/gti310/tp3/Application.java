26d81bc381c740e523b2bac986193ae792439dc2
Vincent Boiteau-Robert <vboiteau94@gmail.com>
2016-03-03 13:56:09 -0500

    Add the default documents for lab03

---
+++ src/gti310/tp3/Application.java
@@ -0,0 +1,23 @@
+package gti310.tp3;
+
+/**
+ * The Application class defines a template method to call the elements to
+ * solve the problem Unreal-Networks is façing.
+ * 
+ * @author François Caron <francois.caron.7@ens.etsmtl.ca>
+ */
+public class Application {
+
+	/**
+	 * The Application's entry point.
+	 * 
+	 * The main method makes a series of calls to find a solution to the
+	 * problem. The program awaits two arguments, the complete path to the
+	 * input file, and the complete path to the output file.
+	 * 
+	 * @param args The array containing the arguments to the files.
+	 */
+	public static void main(String args[]) {
+		System.out.println("Unreal Networks Solver !");
+	}
+}
